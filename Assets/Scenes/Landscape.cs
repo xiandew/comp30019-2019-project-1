e8138e8600d423fcf1404dd0f4c6b01526f8d870
Mingyu Su <msu2@student.unimelb.edu.au>
2019-08-30 14:54:24 +1000

    Updates:
    - Setup a basic square for the basis of landscape.
    - Setup a basic shader to render the colors of the landscape.

---
+++ Assets/Scenes/Landscape.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+public class Landscape : MonoBehaviour {
+
+    public int xLength;
+    public int zLength;
+
+    void Start() {
+        MeshFilter landMesh = this.gameObject.AddComponent<MeshFilter>();
+        landMesh.mesh = this.CreateLand();
+
+        MeshRenderer landRender = this.gameObject.AddComponent<MeshRenderer>();
+        landRender.material.shader = Shader.Find("Unlit/LandscapeShader");
+    }
+
+    void Update() {
+
+    }
+
+    Mesh CreateLand() {
+        Mesh land = new Mesh();
+        land.name = "Landscape";
+
+        int numOfVerticies = xLength * zLength * 2 * 3;
+        Vector3[] verticies = new Vector3[numOfVerticies];
+        Color[] colors = new Color[numOfVerticies];
+
+        int count = 0;
+        for (int x = 0; x < xLength; x++) {
+            for (int z = 0; z < zLength; z++) {
+                verticies[count] = new Vector3((float) x, 0, (float) z);
+                verticies[count + 1] = new Vector3((float) x, 0, (float) z + 1);
+                verticies[count + 2] = new Vector3((float) x + 1, 0, (float) z);
+
+                verticies[count + 3] = new Vector3((float) x + 1, 0, (float) z + 1); 
+                verticies[count + 4] = new Vector3((float) x + 1, 0, (float) z); 
+                verticies[count + 5] = new Vector3((float) x, 0, (float) z + 1);
+
+                colors[count] = Color.yellow; 
+                colors[count + 1] = Color.yellow; 
+                colors[count + 2] = Color.yellow; 
+
+                colors[count + 3] = Color.yellow; 
+                colors[count + 4] = Color.yellow; 
+                colors[count + 5] = Color.yellow; 
+
+                count += 6;
+            }
+        }
+
+        land.vertices = verticies;
+        land.colors = colors;
+
+        int[] triangles = new int[land.vertices.Length];
+        for (int i = 0; i < land.vertices.Length; i++) {
+            triangles[i] = i;
+        }
+
+        land.triangles = triangles;
+
+        return land;
+    }
+
+}
